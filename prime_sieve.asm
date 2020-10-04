1f68f742406940ad4098ec6538a3feb3c696feac
Conrad Khakhria <67520159+ConradKhakhria@users.noreply.github.com>
2020-10-04 22:00:13 +0100

    Removed needless comments

--- prime_sieve.asm
+++ prime_sieve.asm
@@ -1,15 +1,10 @@
 ; Sieve of Eratosthenes in Linux x86_64 NASM
-;
-; This uses a list of boolean true/false values with the
-; definitions of the values defined directly below to get
-; a list of the positions of prime numbers and then just
-; prints them with glibc printf()
 
 
 
 PRIME   equ     1
-COMP    equ     0               ;  1 means prime, 0 means composite
-LIMIT   equ     10000000        ; LIMIT: the number to go up to 
+COMP    equ     0
+LIMIT   equ     10000000
 
         global  main
         extern  printf
@@ -22,39 +17,39 @@ bools:  times LIMIT * 2 db PRIME
 main:
         push    rbx
 
-        mov     rcx, 2                  ; the first prime number is 2
-        mov     word [bools], COMP      ; set both 0 and 1 to COMP
+        mov     rcx, 2
+        mov     word [bools], COMP
 
 .loop:
         cmp     byte [bools + rcx], PRIME
         call    mul_fn
 
         inc     rcx
-        cmp     rcx, LIMIT              ; check if we're at the limit
+        cmp     rcx, LIMIT
         je      print_primes
 
         jmp     .loop
 
 mul_fn: 
-        mov     rax, rcx                ; moves out current prime number to the rax register
+        mov     rax, rcx
         shl     rax, 1
 .loop:
-        mov     byte [bools + rax], COMP        ; set the multiple as composite
-        add     rax, rcx                        ; step up by the original prime
+        mov     byte [bools + rax], COMP
+        add     rax, rcx
 
-        cmp     rax, LIMIT      ; check if we're at the limit
-        jl      .loop           ; if less than the limit, loop again
+        cmp     rax, LIMIT
+        jl      .loop
 
-        ret                     ; return from the function to out position in main.loop
+        ret
 
 print_primes:
         xor     rcx, rcx
 .loop:
-        cmp     rcx, LIMIT      ; check if we're at the limit
+        cmp     rcx, LIMIT
         je      exit
 
-        inc     rcx                             ; go to the next number
-        cmp     byte [bools + rcx], COMP        ; check if it's composite
+        inc     rcx
+        cmp     byte [bools + rcx], COMP
         je      .loop
 
         push    rcx
